70cc383a792d4c24e05c7d70b550378c0608edad
islem alibi <islem.alibii@gmail.com>
2025-02-17 13:55:28 +0100

    code updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 import LoginPage from '@/views/LoginPage.vue';
+import SignupPage from '@/views/SignupPage.vue';
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -15,6 +17,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     component: LoginPage
+  },
+  {
+    path: '/signup',
+    component: SignupPage
   },
 ]
 

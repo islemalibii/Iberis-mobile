a6ffd2b3edae376eaae412dfbcda9c4d764c93c4
MariemTaouai <maryemtaouai57@gmail.com>
2025-02-21 01:52:04 +0100

    fazet lfacebook wl gmail

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,154 +1,319 @@
 <template>
-    <ion-page>
-      <ion-header>
-      </ion-header>
-      <ion-content>
-        <div class="container">
-          <div class="content-wrapper">
-            <div class="image-container">
-              <img 
-                src="@/assets/logo-iberis.png" 
-                alt="Iberis Logo"
-                class="logo"
-              >
-            </div>
-            <h1 class="heading">Happy To See You Again!</h1>
-            <p class="signup-link">
-              You don't have an account? <a href="/signup">Create an account</a>
-            </p>
-  
-            <ion-list class="list">
-              <ion-item class="item">
-                <ion-img src="@/assets/email.png" class="input-icon"></ion-img>
-                <ion-input type="email" placeholder="Email" class="input-field"></ion-input>
-              </ion-item>
-  
-              <ion-item class="item">
-                <ion-img src="@/assets/cadenas.png" class="input-icon"></ion-img>
-                <ion-input type="password" placeholder="Password" class="input-field"></ion-input>
-              </ion-item>
-            </ion-list>
-  
-            <p class="forgot-password"><a href="/gvg"></a>Forgot your password?</p>
-  
-            <ion-button expand="full" class="login-button" @click="handleLogin">
-              Log In
-            </ion-button>
+  <ion-page>
+    <ion-content>
+      <div class="container">
+        <div class="content-wrapper">
+          <div class="image-container">
+            <a href="/home">
+              <img src="../assets/logo-iberis.png" alt="iberisLogo" class="logo" />
+            </a>
           </div>
+
+          <h1 class="heading">Happy To See You Again!</h1>
+          <p class="signup-link">
+            You don't have an account? <a href="/signup">Create an account</a>
+          </p>
+
+          <div class="socialButtons">
+            <div class="google-button-container">
+              <div id="google-button"></div>
+            </div>
+
+            <div class="facebook-button-container">
+              <div id="facebook-button">
+                <div
+                  class="fb-login-button"
+                  data-size="large"
+                  data-layout="default"
+                  data-auto-logout-link="false"
+                  data-use-continue-as="true"
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <p class="or">Or</p>
+          <ion-list class="list">
+            <ion-item class="item">
+              <ion-img src="/src/assets/email.png" class="input-icon"></ion-img>
+              <ion-input
+                type="email"
+                placeholder="Email"
+                :value="email"
+                @ionInput="email = $event.target.value.trim()"
+                class="input-field"
+              ></ion-input>
+            </ion-item>
+            <p v-if="emailError" class="error">{{ emailError }}</p>
+
+            <ion-item class="item">
+              <ion-img src="/src/assets/cadenas.png" class="input-icon"></ion-img>
+              <ion-input
+                type="password"
+                placeholder="Password"
+                :value="password"
+                @ionInput="password = $event.target.value.trim()"
+                class="input-field"
+              ></ion-input>
+            </ion-item>
+            <p v-if="passwordError" class="error">{{ passwordError }}</p>
+          </ion-list>
+
+          <p class="forgot-password"><a href="#">Forgot your password?</a></p>
+
+          <ion-button expand="full" class="login-button" @click="handleLogin">
+            Log In
+          </ion-button>
+
+          <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
         </div>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { IonPage, IonHeader, IonContent, IonItem, IonInput, IonList, IonImg, IonButton } from '@ionic/vue';
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  const email = ref('');
-  const password = ref('');
-  
-  const handleLogin = () => {
-    console.log('Email:', email.value);
-    console.log('Password:', password.value);
-  
-    router.push('/dashboard'); 
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { login } from '@/services/authentification';
+
+const router = useRouter();
+const email = ref('');
+const password = ref('');
+const errorMessage = ref('');
+const emailError = ref('');
+const passwordError = ref('');
+
+const handleLogin = async () => {
+  emailError.value = '';
+  passwordError.value = '';
+  errorMessage.value = '';
+
+  const trimmedEmail = email.value.trim();
+  const trimmedPassword = password.value.trim();
+
+  if (!trimmedEmail) {
+    emailError.value = 'Email field cannot be empty!';
+  }
+  if (!trimmedPassword) {
+    passwordError.value = 'Password field cannot be empty!';
+  }
+
+  if (emailError.value || passwordError.value) {
+    return; 
+  }
+
+  try {
+    const response = await login({ email: trimmedEmail, password: trimmedPassword });
+
+    if (response.data.userExists) {
+      localStorage.setItem('token', response.data.token);  
+      router.push('/home'); 
+    } else {
+      errorMessage.value = 'No account found. Redirecting to sign-up...';
+      setTimeout(() => {
+        router.push('/signup');
+      }, 2000);
+    }
+  } catch (error) {
+    errorMessage.value = 'Invalid credentials. Please try again.';
+  }
+};
+
+
+const handleGoogleSignUp = (response: any) => {
+  console.log("Google Sign-In Response:", response);
+  const idToken = response.credential;
+
+  fetch("https://preprod-api.iberis.io/ar/api/private/user/google-signIn", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ idToken })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.userExists) {
+        localStorage.setItem('token', data.token); 
+        router.push('/home');  
+      } else {
+        router.push('/signup');
+      }
+    })
+    .catch((err) => console.error("Error:", err));
+};
+
+
+const handleFacebookLogin = (response: any) => {
+  if (response.status === "connected") {
+    console.log("Facebook Sign-In Response:", response);
+    const { accessToken } = response.authResponse;
+
+    fetch("https://preprod-api.iberis.io/ar/api/private/user/facebook-signIn", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ accessToken })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.userExists) {
+          localStorage.setItem('token', data.token);  
+          router.push('/home');  
+        } else {
+          router.push('/signup');
+        }
+      })
+      .catch((err) => console.error("Error:", err));
+  } else {
+    console.error("Facebook login failed", response);
+  }
+};
+
+onMounted(() => {
+  const googleScript = document.createElement("script");
+  googleScript.src = "https://accounts.google.com/gsi/client";
+  googleScript.async = true;
+  googleScript.onload = () => {
+    if (!document.getElementById("google-button")?.hasChildNodes()) {
+      window.google?.accounts.id.initialize({
+        client_id: "543531980890-o7btiuq1iod2423htc4c3av4i6l4j28h.apps.googleusercontent.com",
+        callback: handleGoogleSignUp,
+        ux_mode: "popup",
+      });
+
+      window.google.accounts.id.renderButton(
+        document.getElementById("google-button"),
+        { theme: "outline", size: "large", width: "150" }
+      );
+    }
   };
-  </script>
-  
-  <style scoped>
-  ion-content {
-    --background: white;
-  }
-  
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    padding: 24px;
-  }
-  
-  .content-wrapper {
-    max-width: 400px;
-    width: 100%;
-    text-align: center;
-  }
-  
-  .image-container {
-    text-align: center;
-    margin-bottom: 32px;
-  }
-  
-  .logo {
-    height: 160px;
-    width: 260px;
-  }
-  
-  .heading {
-    font-size: 1.7rem;
-    line-height: 1.8;
-    color:  #47463D;
-    margin-bottom: 1.5rem;
-    font-weight: 550;
-  }
-  
-  .signup-link {
-    font-size: 0.9rem;
-    color: #636e72;
-    margin-bottom: 2rem;
-  }
-  
-  .signup-link a {
-    color: #DFC925;
-    text-decoration: none;
-    font-weight: 600;
-  }
-  
-  .list {
-    background: transparent;
-    margin-bottom: 1.8rem;
+  document.head.appendChild(googleScript);
+
+  const facebookScript = document.createElement("script");
+  facebookScript.src = "https://connect.facebook.net/en_US/sdk.js";
+  facebookScript.async = true;
+  facebookScript.onload = () => {
+    window.FB.init({
+      appId: "507777845320852",
+      cookie: true,
+      xfbml: true,
+      version: "v15.0"
+    });
+
+    window.FB.Event.subscribe("auth.statusChange", handleFacebookLogin);
+    window.FB.XFBML.parse();
+  };
+  document.head.appendChild(facebookScript);
+});
+</script>
+
+<style scoped>
+ion-content {
+  --background: white;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+}
+
+.content-wrapper {
+  max-width: 400px;
+  width: 100%;
+  text-align: center;
+}
+
+.image-container {
+  text-align: center;
+  margin-bottom: 32px;
+}
+
+.logo {
+  height: 160px;
+  width: 260px;
+}
+
+.heading {
+  font-size: 1.7rem;
+  line-height: 1.8;
+  color: #47463D;
+  margin-bottom: 1.5rem;
+  font-weight: 550;
+}
+
+.signup-link {
+  font-size: 0.9rem;
+  color: #636e72;
+  margin-bottom: 2rem;
+}
+
+.signup-link a {
+  color: #DFC925;
+  text-decoration: none;
+  font-weight: 600;
+}
+
+.list {
+  background: transparent;
+  margin-bottom: 1.8rem;
 }
 
 .item {
-    --background: #cab40f; 
-    --border-radius: 12px;
-    --padding-start: 16px;
-    --inner-padding-end: 16px;
-    margin-bottom: 32px; 
-}
-
-.item:first-child {
-    margin-top: 20px; 
+  --background: #cab40f;
+  --border-radius: 12px;
+  --padding-start: 16px;
+  --inner-padding-end: 16px;
+  margin-bottom: 32px;
 }
 
 .input-icon {
-    width: 24px;
-    height: 24px;
-    margin-right: 12px;
+  width: 24px;
+  height: 24px;
+  margin-right: 12px;
 }
 
 .input-field {
-    --padding-start: 0;
+  --padding-start: 0;
 }
-  .forgot-password {
-    font-size: 0.9rem;
-    color: #cab40f;
-    margin-bottom: 2rem;
-  }
-  .login-button {
-    --background: #47463D; 
-    --color: #ffffff; 
-    font-weight: 550;
-    margin: 90px auto 0; 
-    --padding-top: 20px; 
-    --padding-bottom: 20px; 
-    width: 210px; 
-    display: block; 
+
+.forgot-password {
+  font-size: 0.9rem;
+  color: #cab40f;
+  margin-bottom: 2rem;
+}
+
+.login-button {
+  --background: #47463D;
+  --color: #ffffff;
+  font-weight: 550;
+  margin: 90px auto 0;
+  --padding-top: 20px;
+  --padding-bottom: 20px;
+  width: 210px;
+  display: block;
 }
 
 .login-button::part(native) {
-    border-radius: 20px; 
+  border-radius: 20px;
 }
-  </style>
+
+.error {
+  color: red;
+  margin-top: 10px;
+}
+
+.socialButtons {
+  display: flex;
+  gap: 15px;
+  justify-content: center;
+  margin-bottom: 20px;
+  margin-top: 30px;
+}
+
+.or {
+  margin: 10px 0;
+  font-weight: bold;
+  color: #47463D;
+}
+</style>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,100 +1,204 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true" class="ion-padding">
-    <a href="/home">     
-      <img src="@/assets/logo-iberis.png" alt="iberisLogo" class="logo" />
-    </a>
-    <div class="signup-container">
-      <h1>Welcome to the family !</h1>
-      <p>You already have an account? <a href="/login">Log in</a></p>
+      <a href="/home">
+        <img src="@/assets/logo-iberis.png" alt="iberisLogo" class="logo" />
+      </a>
+      <div class="signup-container">
+        <h1>Welcome to the family !</h1>
+        <p>You already have an account? <a href="/login">Log in</a></p>
 
-      <div class="socialButtons">
+        <div class="socialButtons">
+          <div class="google-button-container">
+            <div id="google-button"></div>
+          </div>
 
-        <div class="googleButton-container">
-          <div id="google-button" class="socialBtn"></div>
+          <div class="facebook-button-container">
+            <div id="facebook-button">
+              <div 
+                class="fb-login-button" 
+                data-size="large" 
+                data-layout="default" 
+                data-auto-logout-link="false" 
+                data-use-continue-as="true">
+              </div>
+            </div>
+          </div>
         </div>
 
-        <ion-button expand="block" fill="outline" class="socialBtn">
-          <ion-icon slot="start" name="logo-facebook"></ion-icon> Facebook
-        </ion-button>
-      </div>
-      <p class="or">Or</p>
+        <p class="or">Or</p>
 
         <ion-list class="list">
           <ion-item class="item">
-            <ion-input :value="fullname" @ionInput="fullname = $event.target.value" placeholder="Fullname"></ion-input>
+            <ion-input
+              :value="fullname"
+              @ionInput="fullname = $event.target.value"
+              placeholder="Fullname"
+            ></ion-input>
           </ion-item>
           <ion-item class="item">
-            <ion-input :value="email" @ionInput="email = $event.target.value" type="email" placeholder="Email"></ion-input>
+            <ion-input
+              :value="email"
+              @ionInput="email = $event.target.value"
+              type="email"
+              placeholder="Email"
+            ></ion-input>
           </ion-item>
           <ion-item class="item">
-            <ion-input :value="password" @ionInput="password = $event.target.value" type="password" placeholder="Password" clearInput="true"></ion-input>
+            <ion-input
+              :value="password"
+              @ionInput="password = $event.target.value"
+              type="password"
+              placeholder="Password"
+              clearInput="true"
+            ></ion-input>
           </ion-item>
         </ion-list>
 
-
         <div class="checkboxContainer">
-          <ion-checkbox :checked="acceptTerms" @ionChange="acceptTerms = $event.detail.checked"></ion-checkbox>
-          <p class="conditions" >By signing up, you accept the <a href="#">Utilisation Conditions</a></p>
+          <ion-checkbox
+            :checked="acceptTerms"
+            @ionChange="acceptTerms = $event.detail.checked"
+          ></ion-checkbox>
+          <p class="conditions">
+            By signing up, you accept the <a href="#">Utilisation Conditions</a>
+          </p>
         </div>
 
         <div class="buttonContainer">
           <ion-button expand="block" @click="Signup" class="signupBtn">Sign up</ion-button>
         </div>
+        <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+
       </div>
     </ion-content>
   </ion-page>
-  </template>
-
+</template>
 
 <script setup>
-  import { ref, onMounted } from "vue";
-  import { signup } from "@/services/authentification";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router"; 
+import { signup } from "@/services/authentification";
 
-  const fullname = ref("");
-  const email = ref("");
-  const password = ref("");
-  const acceptTerms = ref(false);
+const fullname = ref("");
+const email = ref("");
+const password = ref("");
+const acceptTerms = ref(false);
+const errorMessage = ref('');
+const router = useRouter();  
 
-  const Signup = async () => {
+const checkConditions = (socialSignup = false) => {
+  if (!socialSignup) {
     if (!fullname.value || !email.value || !password.value) {
-      console.error("Please fill in all fields");
-      return;
+      errorMessage.value = "Please fill in all fields";
+      return false;
     }
     if (!acceptTerms.value) {
-      console.error("You must accept the terms and conditions");
-      return;
+      errorMessage.value = "You must accept the terms and conditions";
+      return false;
     }
+  }
+  return true;
+};
 
-    try {
-      const response = await signup({
-        name: fullname.value,
-        email: email.value,
-        password: password.value,
-      });
-      console.log("Signup successful:", response.data);
-    } catch (error) {
-      console.error("Signup failed:", error);
-    }
-  };
 
-  const handleGoogleSignUp = (response) => {
-    console.log("Google Sign-In Response:", response);
-    const idToken = response.credential;
 
-    fetch("https://preprod-api.iberis.io/ar/api/private/user/google-signup", {
+const Signup = async () => {
+  console.log("Signup function triggered");
+  if (!checkConditions()) return;
+
+  try {
+    const response = await signup({
+      name: fullname.value,
+      email: email.value,
+      password: password.value,
+    });
+    console.log("Signup successful:", response.data);
+    errorMessage.value = "Signup successful!";
+    router.push('/login');  
+  } catch (error) {
+    console.error("Signup failed:", error);
+    errorMessage.value = "Signup failed: " + error.message;
+  }
+};
+
+const handleGoogleSignUp = (response) => {
+  console.log("Google Sign-In Response:", response);
+
+  if (!checkConditions(true)) return; 
+
+  const idToken = response.credential;
+
+  fetch("https://preprod-api.iberis.io/ar/api/private/user/google-signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ idToken }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("HTTP error " + res.status); 
+      }
+      return res.text(); 
+    })
+    .then((text) => {
+      console.log("Raw response from server:", text); 
+      try {
+        const data = JSON.parse(text); 
+        console.log("Parsed server response:", data);
+        errorMessage.value = "Signup successful!";
+        router.push("/home");
+      } catch (e) {
+        console.error("Error parsing JSON:", e);
+        errorMessage.value = "Signup failed: Invalid JSON response.";
+      }
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+      errorMessage.value = "Signup failed: " + err.message;
+    });
+};
+
+
+
+
+
+const handleFacebookLogin = (response) => {
+  if (response.status === "connected") {
+    console.log("Facebook Sign-In Response:", response);
+
+    if (!checkConditions(true)) return; 
+
+    const { accessToken } = response.authResponse;
+
+    fetch("https://preprod-api.iberis.io/ar/api/private/user/facebook-signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ idToken }),
+      body: JSON.stringify({ accessToken }),
     })
       .then((res) => res.json())
-      .then((data) => console.log("Server response:", data))
-      .catch((err) => console.error("Error:", err));
-  };
+      .then((data) => {
+        console.log("Server response:", data);
+        errorMessage.value = "Signup successful!";
+        router.push("/home"); 
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        errorMessage.value = "Signup failed: " + err.message;
+      });
+  } else {
+    console.error("Facebook login failed", response);
+    errorMessage.value = "Facebook login failed.";
+  }
+};
 
-  onMounted(() => {
-    if (window.google?.accounts) {
-      window.google.accounts.id.initialize({
+
+onMounted(() => {
+  const googleScript = document.createElement("script");
+  googleScript.src = "https://accounts.google.com/gsi/client";
+  googleScript.async = true;
+  googleScript.onload = () => {
+    if (!document.getElementById("google-button").hasChildNodes()) {
+      window.google?.accounts.id.initialize({
         client_id: "543531980890-o7btiuq1iod2423htc4c3av4i6l4j28h.apps.googleusercontent.com",
         callback: handleGoogleSignUp,
         ux_mode: "popup",
@@ -102,21 +206,39 @@
 
       window.google.accounts.id.renderButton(
         document.getElementById("google-button"),
-        { theme: "outline", size: "large", width: "90" }
+        { theme: "outline", size: "large", width: "150" }
       );
     }
-  });
-
-  const GoogleSignup = () => {
-    window.google.accounts.id.prompt();
   };
+  document.head.appendChild(googleScript);
+
+  const facebookScript = document.createElement("script");
+  facebookScript.src = "https://connect.facebook.net/en_US/sdk.js";
+  facebookScript.async = true;
+  facebookScript.onload = () => {
+    console.log("Facebook SDK loaded successfully");
+
+    if (window.FB) {
+      console.log("FB object exists");
+      window.FB.init({
+        appId: "507777845320852",
+        cookie: true,
+        xfbml: true,
+        version: "v15.0"
+      });
+
+      window.FB.Event.subscribe("auth.statusChange", handleFacebookLogin);
+      window.FB.XFBML.parse();
+    } else {
+      console.error("FB object not found");
+    }
+  };
+  document.head.appendChild(facebookScript);
+});
 </script>
 
-
-
-
 <style scoped>
-.ion-padding{
+.ion-padding {
   --background: white;
 }
 .signup-container {
@@ -130,13 +252,13 @@
 }
 
 h1 {
-font-size: 40px;
-  color: #47463D;
+  font-size: 40px;
+  color: #47463d;
 }
 
 p {
   font-size: 15px;
-  color: #47463D;
+  color: #47463d;
 }
 .socialButtons {
   display: flex;
@@ -144,83 +266,52 @@ p {
   justify-content: center;
   margin-bottom: 20px;
   margin-top: 30px;
-
 }
-
-.socialBtn {
-  flex: 1;
-  font-size: 16px;
-  text-transform: none;
-  color: #47463D;
-  --border-color: #47463D;
-  --border-width: 2px; 
-  --border-radius: 10px; 
-}
-
-.googleButton-container{
-  border: 1px solid black;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  width: 190px; 
-  overflow: hidden;
-}
-
-#google-button {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-
 
 .or {
   margin: 10px 0;
   font-weight: bold;
-  color: #47463D;
+  color: #47463d;
 }
 
-.list{
+.list {
   background: transparent;
 }
 .item {
-  --background: #DFC925;
-  --border-radius: 10px; 
-  --border-color: #47463D;
-  --padding-start: 15px; 
-  --inner-padding-end: 15px; 
+  --background: #dfc925;
+  --border-radius: 10px;
+  --border-color: #47463d;
+  --padding-start: 15px;
+  --inner-padding-end: 15px;
   margin-bottom: 20px;
-  margin-top: 15px; 
- 
+  margin-top: 15px;
 }
 
 .checkboxContainer {
   display: flex;
-  align-items: center; 
-  gap: 8px; 
+  align-items: center;
+  gap: 8px;
 }
-.conditions{
+.conditions {
   font-size: 15px;
-  color: #47463D;
+  color: #47463d;
 }
-
-
 
 .buttonContainer {
-  margin-top:50px;
+  margin-top: 50px;
   display: flex;
   justify-content: center;
 }
 .signupBtn {
-  --background: #47463D;
+  --background: #47463d;
   --color: white;
-  --border-radius:8px;
-  height:35px;
-  width:150px;
-
+  --border-radius: 8px;
+  height: 35px;
+  width: 150px;
+}
+.facebook-button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 0px;
 }
 </style>
